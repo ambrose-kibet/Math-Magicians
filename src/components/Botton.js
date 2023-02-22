2888819a5c72c7a5e6d35a7c5d33b7da688803bd
ambrose-kibet <ambrosekibet36@gmail.com>
2023-02-22 13:09:57 +0300

    created calculator Ui

--- src/components/Botton.js
+++ src/components/Botton.js
@@ -3,7 +3,11 @@ import PropTypes from 'prop-types';
 
 function Botton({ item, id = 25 }) {
   return (
-    <button className={id % 4 === 0 ? 'btn btn-orange ' : 'btn'} type="button">
+    <button
+      className={id % 4 === 0 ? 'btn btn-orange ' : 'btn'}
+      type="button"
+      data-name={item}
+    >
       {item}
     </button>
   );
