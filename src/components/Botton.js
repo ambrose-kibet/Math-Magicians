04df3477c263e97e29a5ee6c529940cc1982c3ae
ambrose-kibet <ambrosekibet36@gmail.com>
2023-02-22 15:41:56 +0300

    add calc functionality

--- src/components/Botton.js
+++ src/components/Botton.js
@@ -1,11 +1,12 @@
 import PropTypes from 'prop-types';
 
-function Botton({ item, id = 25 }) {
+function Botton({ item, id = 25, handleClick }) {
   return (
     <button
       className={id % 4 === 0 ? 'btn btn-orange ' : 'btn'}
       type="button"
       data-name={item}
+      onClick={(e) => handleClick(e)}
     >
       {item}
     </button>
@@ -14,5 +15,6 @@ function Botton({ item, id = 25 }) {
 Botton.propTypes = {
   item: PropTypes.string.isRequired,
   id: PropTypes.number.isRequired,
+  handleClick: PropTypes.func.isRequired,
 };
 export default Botton;
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,19 +1,41 @@
+import { useState } from 'react';
+import calculate from '../logic/calculate';
 import buttons from '../utils/buttons';
 import Button from './Botton';
 
-const Calculator = () => (
-  <section className="calculator">
-    <input type="text" className="display" />
-    <div className="button-container">
-      {buttons.map((button) => (
-        <Button key={button.id} item={button.item} id={button.id} />
-      ))}
-    </div>
-    <div className="rest-btns">
-      <Button item="0" id={21} />
-      <Button item="." id={22} />
-      <Button item="=" id={24} />
-    </div>
-  </section>
-);
+const Calculator = () => {
+  const [calc, setcalc] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
+  const handleClick = (e) => {
+    const { name } = e.target.dataset;
+    setcalc(calculate(calc, name));
+  };
+  return (
+    <section className="calculator">
+      <input
+        type="text"
+        className="display"
+        value={calc.next || calc.total || 0}
+      />
+      <div className="button-container">
+        {buttons.map((button) => (
+          <Button
+            key={button.id}
+            item={button.item}
+            id={button.id}
+            handleClick={handleClick}
+          />
+        ))}
+      </div>
+      <div className="rest-btns">
+        <Button item="0" id={21} handleClick={handleClick} />
+        <Button item="." id={22} handleClick={handleClick} />
+        <Button item="=" id={24} handleClick={handleClick} />
+      </div>
+    </section>
+  );
+};
 export default Calculator;
