3e9a5954561d9a5b64d42e395974771c4be500d2
ambrose-kibet <ambrosekibet36@gmail.com>
2023-02-28 15:20:24 +0300

    add Navbar

--- src/Pages/Quote.js
+++ src/Pages/Quote.js
@@ -1,7 +1,7 @@
 const Quote = () => (
   <section className="section-container">
     <p>
-      Mathematic is not about numbers equations, computation or algorithms its
+      Mathematics is not about numbers equations, computation or algorithms its
       about understanding - William Paul Thurston
     </p>
   </section>
